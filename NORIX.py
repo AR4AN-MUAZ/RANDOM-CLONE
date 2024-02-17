8393b9bb00e011473fc10e8f39840059999d39ea
AR4AN_-_KHALIFA <129208285+AR4AN-MUAZ@users.noreply.github.com>
2024-02-17 21:36:40 +0600

    Update NORIX.py

--- NORIX.py
+++ NORIX.py
@@ -1,1 +1,7 @@
-import mainnorix
+#!/data/data/com.termux/files/usr/bin/python
+import platform,os,sys
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import mainnorix
+elif bit == '32bit':
+    print(" [#] Sorry bit is not frequent right now")
